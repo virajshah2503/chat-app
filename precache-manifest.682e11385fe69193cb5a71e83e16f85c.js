dfc4abef7a7223cf33bebb6d66a83f7a0f216109
unknown <viraj>
2019-09-17 16:31:55 +0530

    Updates

---
+++ precache-manifest.682e11385fe69193cb5a71e83e16f85c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47020aa8c55b063c763c2bc55558f155",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "ae6c7a00933a80515bf3",
+    "url": "/chat-app/static/css/main.0b41e38d.chunk.css"
+  },
+  {
+    "revision": "359b340a81a47671ecf1",
+    "url": "/chat-app/static/js/2.c9956930.chunk.js"
+  },
+  {
+    "revision": "ae6c7a00933a80515bf3",
+    "url": "/chat-app/static/js/main.b849b694.chunk.js"
+  },
+  {
+    "revision": "8d6614215b9c957299b0",
+    "url": "/chat-app/static/js/runtime~main.711bca32.js"
+  }
+]);
