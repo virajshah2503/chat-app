eb0d3167614983ef9b67e864eabfecfed5c259d5
unknown <viraj>
2019-09-17 15:56:56 +0530

    message key utility

--- src/constants/defaultState.js
+++ src/constants/defaultState.js
@@ -1,6 +1,6 @@
 export const defaultState = {
     messages : {
-        'viraj_shah-chat_bot' : []
+        'viraj_shah-chat_bot' : [] // key format is 'from.id-to.id'
     },
     from : { id : 'viraj_shah', name : 'Viraj Shah'},
     to : { id : 'chat_bot', name : 'Chat Bot'}
